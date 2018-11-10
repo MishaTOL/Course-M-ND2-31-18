94223d569178a839648b6da8cc017b6caae752f6
akaravitski <akaravitski@servicechannel.com>
2018-11-11 00:22:30 +0300

    Added Lab3 from Korovitskiy

---
+++ Korovitskiy/Lab3/SuperDuperTripleAsholeDiC/ExampleConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using SuperDuperTripleAsholeDiC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registrator.RegisteringDependency<ITestInterface, TestClass2>();
+
+            var obj = Resolver.GetImplementation<ITestInterface>();
+            Console.WriteLine(obj.GetString());
+            Console.Read();
+        }
+    }
+}

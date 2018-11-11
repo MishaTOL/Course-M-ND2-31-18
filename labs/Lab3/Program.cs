1176db9587bc499c662fa88c7d455f76991f5938
MuhortovAA <xbestya@mail.ru>
2018-11-11 20:35:21 +0300

    Lab3 DI  Container+ Resolver

---
+++ labs/Lab3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace Lab3
+{
+    //UI Layer
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IUnityContainer objContainer = new UnityContainer();
+            objContainer.RegisterType<Customer>();
+            objContainer.RegisterType<IDal, MSSQLDal>();
+            objContainer.RegisterType<IDal, OracleDal>();
+            Customer obj = objContainer.Resolve<Customer>();
+            obj.CustomerName = "test1";
+            //Customer obj = new Customer(new MSSQLDal()) { CustomerName = "test1" };
+            obj.Add(obj.CustomerName);
+
+        }
+    }
+    //Middle layer
+    public class Customer
+    {
+        private IDal Odal;
+        public string CustomerName { get; set; }
+        public Customer(IDal iobj)
+        {
+            Odal = iobj;
+        }
+        public void Add(string str)
+        {
+            //DAL
+
+            Odal.Add(str);
+        }
+    }
+
+    //DAL
+    public interface IDal
+    {
+        void Add(string str);
+    }
+    public class MSSQLDal : IDal
+    {
+        public void Add(string str)
+        {
+
+            Console.WriteLine($"{str}:MSSQL Inserted!");
+            Console.ReadLine();
+        }
+    }
+    public class OracleDal : IDal
+    {
+        public void Add(string str)
+        {
+
+            Console.WriteLine($"{str}:Oracle Inserted!");
+            Console.ReadLine();
+        }
+    }
+}

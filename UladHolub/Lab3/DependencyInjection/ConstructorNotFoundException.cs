837ded70fff59ef1957fea75210256a01067752d
Uladzislau Holub <uladzislau.holub@gmail.com>
2018-11-13 13:17:23 +0300

    Added Lab3

---
+++ UladHolub/Lab3/DependencyInjection/ConstructorNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    [Serializable]
+    class ConstructorNotFoundException : Exception
+    {
+        public ConstructorNotFoundException()
+        {
+
+        }
+
+        public ConstructorNotFoundException(string name)
+            : base(String.Format("Suitable constructor for type {0} not found", name))
+        {
+
+        }
+
+    }
+}
---
+++ UladHolub/Lab3/Lab3/Services/Student1.cs
@@ -0,0 +1,13 @@
+﻿using Lab3.Interfaces;
+using System;
+
+namespace Lab3.Services
+{
+    public class Student1 : IStudent
+    {
+        public void Write()
+        {
+            Console.WriteLine("It's object of Student1 class!");
+        }
+    }
+}

708a21d71a2a6ce098233813c46faf09979d29ce
Kirill Boika <boika.kv@yandex.by>
2018-11-13 20:02:57 +0300

    Added Lab_1

---
+++ Boika/Lab_1/Lab_1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab_1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}

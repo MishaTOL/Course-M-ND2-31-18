aa6358ac5c1132f017d89431e1b0ede34f4f7fc9
Uladzislau Komar <komarvlad15799@mail.ru>
2018-11-13 20:03:05 +0300

    Lab3 Komar

---
+++ Uladzislau Komar/Lab3/Lab3.DI/Lab3.DI.Tests/Fakes/FakeStudent.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab3.DI.Tests.Fakes
+{
+    class FakeStudent : IFakeStudent
+    {
+        public string FirstName { get; set; }
+
+        public FakeStudent()
+        {
+
+        }
+
+        public FakeStudent(string firstName)
+        {
+            this.FirstName = firstName;
+        }
+
+
+        public string Do()
+        {
+            return "Hello";
+        }
+    }
+}

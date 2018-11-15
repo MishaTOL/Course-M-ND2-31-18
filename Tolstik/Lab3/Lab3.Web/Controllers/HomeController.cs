51d59dbd143efdea108487318899d0ca6e8ae844
MishaTOL <misha.tol56@gmail.com>
2018-11-15 09:42:20 +0300

    "Add Lab3"

---
+++ Tolstik/Lab3/Lab3.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Lab3.Data.Contracts.Repositories;
+using Lab3.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab3.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = Container.Resolve<IStudentRepository>();
+            ViewBag.Students = repo.FindAllStudents();
+            return View();
+        }
+    }
+}
---
+++ Tolstik/Lab3/Lab3.Web/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using Lab3.Data.Contracts.Repositories;
+using Lab3.Infrastructure;
+using Lab3.Repositories;
+using Lab3.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Lab3.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Container.Register<IStudentRepository, StudentRepository>();
+        }
+    }
+}

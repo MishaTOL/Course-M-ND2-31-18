484c54241b7b27f6077fe255029f512eb61067c1
akaravitski <akaravitski@servicechannel.com>
2018-11-26 00:56:13 +0300

    added Lab6 from Korovitskiy

---
+++ Korovitskiy/Lab6/Aligator/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aligator.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string groupsId)
+        {
+            if (string.IsNullOrEmpty(groupsId))
+                return;
+
+            var isRight = GameHub.CheckWord(message, groupsId);
+            await this.Clients.All.SendAsync("Send", message, isRight);
+        }
+    }
+}

83cc31c54bd40eee5f12b8a65f93ba04b0d8fc7a
UladH <uladzislau.holub@gmail.com>
2018-11-28 16:16:45 +0300

    Delete russian comments

--- UladHolub/Lab4/Data.Repositories/Identity/EmailService.cs
+++ UladHolub/Lab4/Data.Repositories/Identity/EmailService.cs
@@ -8,19 +8,15 @@ namespace Data.Repositories.Identity
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // настройка логина, пароля отправителя
             var from = "example@gmail.com";
             var pass = "password";
 
-            // адрес и порт smtp-сервера, с которого мы и будем отправлять письмо
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
 
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
             client.Credentials = new System.Net.NetworkCredential(from, pass);
             client.EnableSsl = true;
-
-            // создаем письмо: message.Destination - адрес получателя
             var mail = new MailMessage(from, message.Destination);
             mail.Subject = message.Subject;
             mail.Body = message.Body;

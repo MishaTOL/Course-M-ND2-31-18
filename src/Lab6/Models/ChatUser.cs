e471bb035694a0a483ca60f10b47dce3787f3130
MuhortovAA <xbestya@mail.ru>
2018-12-01 10:14:35 +0300

    update lab6

---
+++ src/Lab6/Models/ChatUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.Models
+{
+    public class ChatUser
+    {
+        public string UserName { get; set; }
+    }
+}

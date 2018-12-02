783f29c98bce3e74fb3339792e845d31d59e4d17
Mikhail Verbitski <misha.verbi@gmail.com>
2018-12-02 17:10:54 +0300

    Created simple crocodile

---
+++ Verbitsky/Lab6Crocodile/Lab6Crocodile/Hubs/CanvasHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6Crocodile.Hubs
+{
+    public class CanvasHub : Hub
+    {
+        public async Task MouseDown(int x, int y)
+        {
+            await this.Clients.All.SendAsync("MouseDown", x, y);
+        }
+        public async Task MouseMove(int x, int y)
+        {
+            await this.Clients.All.SendAsync("MouseMove", x, y);
+        }
+        public async Task MouseUp(int x, int y)
+        {
+            await this.Clients.All.SendAsync("MouseUp", x, y);
+        }
+    }
+}
---
+++ Verbitsky/Lab6Crocodile/Lab6Crocodile/wwwroot/js/ChatConnection.js
@@ -0,0 +1,18 @@
+﻿const Connection = new signalR.HubConnectionBuilder()
+    .withUrl('/chat')
+    .build();
+
+Connection.on("Send", function (data) {
+    let elem = document.createElement("p");
+    elem.appendChild(document.createTextNode(data));
+    let firstElem = document.getElementById("chatroom").firstChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+});
+
+document.getElementById("sendBtn").addEventListener("click", function (e) {
+    let message = document.getElementById("message").value;
+    document.getElementById("message").value = "";
+    Connection.invoke("Send", message);
+});
+
+Connection.start();

e049ca1e05f7c16bd42c517a940fbb34b4e9b50b
Uladzislau Holub <uladzislau.holub@gmail.com>
2018-12-03 23:42:09 +0300

    Added lab5

---
+++ UladHolub/Lab5/Domain.Contracts/Interfaces/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Contracts.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Interfaces
+{
+    public interface IPostService : IDisposable
+    {
+        IEnumerable<PostViewModel> GetLastestRecords(int recordsNumber);
+        Task<IOperationDetails> CreatePostAsync(PostViewModel postViewModel);
+        Task<IOperationDetails> UpdatePostAsync(PostViewModel postViewModel);
+    }
+}

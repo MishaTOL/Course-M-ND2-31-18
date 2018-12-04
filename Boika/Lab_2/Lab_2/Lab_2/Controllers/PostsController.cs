c279350869d9adaba180a7b432114cd193b6ef81
Kirill Boika <boika.kv@yandex.by>
2018-12-04 15:14:58 +0300

    Add Lab_2

---
+++ Boika/Lab_2/Lab_2/Lab_2/Controllers/PostsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab_2.Models;
+
+namespace Lab_2.Controllers
+{
+    public class PostsController : Controller
+    {
+        private DataBaseContext db = new DataBaseContext();
+
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author);
+            return View(posts.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            Post post = db.Posts.Include(a => a.Author).Where(a => a.Id == id).First();
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Created = DateTime.Now;
+                post.Author = db.Students.Find((Session["User"] as Student).Id);
+                post.AuthorId = post.Author.Id;
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Attach(post);
+                var entry = db.Entry(post);
+                entry.Property(a => a.Content).IsModified = true;
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Post post = db.Posts.Include(a => a.Author).Where(a => a.Id == id).First();
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Include(a => a.Author).Where(a => a.Id == id).First();
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Boika/Lab_2/Lab_2/Lab_2/Models/DataBaseContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab_2.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext()
+           : base("name=DataBase")
+        {
+        }
+
+        public IDbSet<Student> Students { get; set; }
+        public IDbSet<Comment> Comments { get; set; }
+        public IDbSet<Post> Posts { get; set; }
+        public IDbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().HasMany<Post>(a => a.Posts).WithRequired(a => a.Author).HasForeignKey(a => a.AuthorId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Student>().HasMany<Comment>(a => a.Comments).WithRequired(a => a.Author).HasForeignKey(a => a.AuthorId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Post>().HasMany<Comment>(a => a.Comments).WithRequired(a => a.Post).HasForeignKey(a => a.PostId);
+
+            modelBuilder.Entity<Post>().HasMany<Tag>(a => a.Tags).WithMany(a => a.Posts).Map(a =>
+            {
+                a.MapLeftKey("TagsId");
+                a.MapRightKey("PostsId");
+                a.ToTable("TagsPosts");
+            });
+        }
+    }
+}

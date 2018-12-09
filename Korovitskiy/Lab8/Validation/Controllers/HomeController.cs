10333facd3b5dc6d30f657f57f2b36c5950d6fb1
akaravitski <akaravitski@servicechannel.com>
2018-12-09 21:47:44 +0300

    Added lab8 from Korovitskiy

---
+++ Korovitskiy/Lab8/Validation/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Pay()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Pay(Payment payment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Pay", payment);
+            }
+            //success
+            return View();
+        }
+    }
+}

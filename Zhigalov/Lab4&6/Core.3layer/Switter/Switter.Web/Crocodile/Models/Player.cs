695464157b57c437370040696bca2c31c7d6a1f5
Alexander Zhigalov <alexandershowbiz@outlook.com>
2018-12-16 17:08:09 +0300

    Lab4&6 added

---
+++ Zhigalov/Lab4&6/Core.3layer/Switter/Switter.Web/Crocodile/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Switter.Web.Crocodile.Models
+{
+    public class Player
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+        public bool Master { get; set; }
+        public string Word { get; set; }
+    }
+}
